4a0688d2ff79dec467e67be19733cc451fb60158
Recep Kurban <kurbanrecep@gmail.com>
2024-09-28 14:19:51 +0300

    Add register

--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -1,12 +1,50 @@
 <template>
     <div>
-register
+        <h1>Kayıt Sayfası</h1>
+        <p><input v-model="name" id="name" name="name" placeholder="kullanıcı adı" /></p>
+        <p><input v-model="email" id="email" name="email" placeholder="email" /></p>
+        <p><input v-model="password" id="password" name="password" placeholder="şifre" /></p>
+        <button @click="register(name,email,password)">Kayıt</button>
     </div>
 </template>
 
 <script>
     export default {
-        
+        name: "Register",
+        data() {
+            return {
+                visibility: false,
+                user: []
+            }
+        },
+        methods: {
+            register: async function (name, email, password) {
+                try {
+                    const requestOptions = {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json"
+                        },
+                        body: JSON.stringify({
+                            name: name,
+                            username: email,
+                            password: password
+                        })
+                    };
+
+                    const response = await fetch("http://localhost:3000/api/v1/user/", requestOptions);
+                    const data = await response.json();
+                    console.log("DATA", data);
+                    this.user = data;
+
+                    if (data.state) {
+                        this.$router.push("/");
+                    }
+                } catch (error) {
+                    console.log(error);
+                }
+            },
+        }
     }
 </script>
 
